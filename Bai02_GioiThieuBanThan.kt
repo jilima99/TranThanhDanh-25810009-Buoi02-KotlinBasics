9b2209b88bfd066c3e582545e6b607f12c842952
jilima99 <113486936+jilima99@users.noreply.github.com>
2026-09-23 22:53:16 +0700

    Add files via upload
    Buoi02 Bai01 gioi thieu

---
+++ Bai02_GioiThieuBanThan.kt
@@ -0,0 +1,10 @@
+// TranThanhDanh - 25810009
+
+fun main() {
+    val hoTen: String = "Trần Thanh Danh"
+    val namSinh: Int = 2003
+
+    val tuoi = 2026 - namSinh
+
+    println("Hello, Tui là $hoTen, năm nay tui $tuoi tuổi.")
+}
